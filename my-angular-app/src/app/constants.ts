9e6362f4b5369423fe2417f6c940d4f0320f00ee
Maria Camila Beltran <30350681+mcbeltran@users.noreply.github.com>
2018-11-18 17:19:42 -0500

    Revert "no me acuerdo"
    This reverts commit 22f8b4745191c4b85bd86a05c669b2b57c448c27, reversing
    changes made to 7251a38d2b7170500d80a3474ce48cab9b4b4410.

--- my-angular-app/src/app/constants.ts
+++ my-angular-app/src/app/constants.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const API_URL = 'https://web-services-juanpablorn30.c9users.io';
-=======
-export const API_URL = 'https://web-services-juanpablorn30.c9users.io';
->>>>>>> 2da0ed64db9b496a913138f98b807d45f1aa95b0
